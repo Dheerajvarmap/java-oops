6b92b1c797bedec51d0435fa454105b70efa8c72
Dheerajvarmap <dheerajvarma8910@gmail.com>
2025-07-30 10:36:09 +0530

    Add files via upload

---
+++ oopspractical/p1.java
@@ -0,0 +1,14 @@
+package oopspractical;
+
+public class p1 
+{
+	public static void main(String[] args)
+	{
+		int a = 5, b = 7;
+		System.out.println("Addition: " + (a + b));
+		System.out.println("Subtraction: " + (a - b));
+		System.out.println("Multipication: " + (a * b));	
+		System.out.println("Division: " + (a / b));
+		System.out.println("Modulus: " + (a % b));	
+	}
+}
